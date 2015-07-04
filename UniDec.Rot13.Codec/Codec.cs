fec4406d2f40e0ceee47a698c9ec5d6d5756209e
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-05 00:29:02 +0200

    Added a sample codec.

---
+++ UniDec.Rot13.Codec/Codec.cs
@@ -0,0 +1,54 @@
+﻿using UniDecAPI;
+
+namespace UniDec.Rot13.Codec
+{
+    public class Codec : ICodec
+    {
+        public string FriendlyName { get { return "ROT13"; } }
+        public string CallName { get { return "rot13"; } }
+
+        public string Decode(string input)
+        {
+            return Transform(input);
+        }
+
+        public string Encode(string input)
+        {
+            return Transform(input);
+        }
+
+        private string Transform(string what)
+        {
+            var array = what.ToCharArray();
+            for (var i = 0; i < array.Length; i++)
+            {
+                var number = (int)array[i];
+
+                if (number >= 'a' && number <= 'z')
+                {
+                    if (number > 'm')
+                    {
+                        number -= 13;
+                    }
+                    else
+                    {
+                        number += 13;
+                    }
+                }
+                else if (number >= 'A' && number <= 'Z')
+                {
+                    if (number > 'M')
+                    {
+                        number -= 13;
+                    }
+                    else
+                    {
+                        number += 13;
+                    }
+                }
+                array[i] = (char)number;
+            }
+            return new string(array);
+        }
+    }
+}
