749d4b17d1dcb6ef15ffffa97bbdf97508d43878
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-04 23:23:13 +0200

    Added CodecExecutor logic and ArgumentParser class.

--- UniDec/CodecExecutor.cs
+++ UniDec/CodecExecutor.cs
@@ -1,13 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UniDecAPI;
 
 namespace UniDec
 {
     class CodecExecutor
     {
+        private List<ICodec> _codecs;
+ 
         public CodecExecutor(List<ICodec> codecs)
         {
-            
+            _codecs = codecs;
+        }
+
+        public void ExecuteDecoder(string codecCallName, string input)
+        {
+            foreach (var codec in _codecs.Where(codec => codec.CallName == codecCallName))
+            {
+                codec.Decode(input);
+                return;
+            }
+            Console.WriteLine("No codec '{0}' found.", codecCallName);
+        }
+
+        public void ExecuteEncoder(string codecCallName, string input)
+        {
+            foreach (var codec in _codecs.Where(codec => codec.CallName == codecCallName))
+            {
+                codec.Encode(input);
+                return;
+            }
+            Console.WriteLine("No codec '{0}' found.", codecCallName);
         }
     }
 }
