3aa42530e891891cd656bdcc9d0ca86747c94364
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-04 23:02:32 +0200

    Modified program logic to reflect earlier changes.

--- UniDec/Program.cs
+++ UniDec/Program.cs
@@ -5,15 +5,24 @@ namespace UniDec
 {
     class Program
     {
+        private const string CodecPath = "./codecs";
+
+        private static CodecExecutor _codecExecutor;
+        private static CodecLoader _codecLoader;
+
         static void Main(string[] args)
         {
             if(!CodecDirectoryExists())
                 Environment.Exit(0);
+
+            _codecLoader = new CodecLoader(CodecPath);
+            _codecExecutor = new CodecExecutor(_codecLoader.LoadCodecs());
+
         }
 
         static bool CodecDirectoryExists()
         {
-            if (!Directory.Exists("./codecs"))
+            if (!Directory.Exists(CodecPath))
             {
                 Console.WriteLine("No codecs detected.");
                 Console.WriteLine("Creating new folder for codecs. You need to drop codec DLLs there to use UniDec.");
