626c82745f01e2ca865ac5a9841bb34df9c1e3c0
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-04 23:00:45 +0200

    Added codec loading logic.

--- UniDec/CodecLoader.cs
+++ UniDec/CodecLoader.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using UniDecAPI;
 
 namespace UniDec
 {
     class CodecLoader
     {
+        private readonly string _path;
+
+        public CodecLoader(string path)
+        {
+            _path = path;
+        }
+
+        public List<ICodec> LoadCodecs()
+        {
+            var filePaths = Directory.GetFiles(_path, "*.Codec.dll");
+
+            return (
+                from path 
+                in filePaths 
+                select Assembly.LoadFrom(path) 
+                into assembly 
+                from type 
+                in assembly.GetExportedTypes() 
+                select ActivateCodec(type)
+            ).ToList();
+        }
+
+        private ICodec ActivateCodec(Type assemblyType)
+        {
+            if (assemblyType.GetInterface(typeof(ICodec).Name) == null)
+                return null;
+
+            return Activator.CreateInstance(assemblyType) as ICodec;
+        }
     }
 }
