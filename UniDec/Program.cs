6f046f88ce08e10b55ebe7c037c00d8d70fd853c
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-04 18:40:33 +0200

    Added codec directory check.

--- UniDec/Program.cs
+++ UniDec/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace UniDec
+﻿using System;
+using System.IO;
+
+namespace UniDec
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CodecDirectoryCheck();
+        }
 
+        static void CodecDirectoryCheck()
+        {
+            if (!Directory.Exists("./codecs"))
+            {
+                Console.WriteLine("No codecs detected.");
+                Console.WriteLine("Creating new folder for codecs. You need to drop codec DLLs there to use UniDec.");
+                Directory.CreateDirectory("./codecs");
+                Environment.Exit(0);
+            }
         }
     }
 }
