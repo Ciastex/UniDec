1ab5bad49cfa2860fc844da12478d17c4ff4dce5
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-05 12:04:12 +0200

    Updated ICodec to contain method using key value for encryption/decryption.

--- UniDecAPI/ICodec.cs
+++ UniDecAPI/ICodec.cs
@@ -7,5 +7,8 @@
 
         string Decode(string input);
         string Encode(string input);
+
+        string Decode(string input, string key);
+        string Encode(string input, string key);
     }
 }
