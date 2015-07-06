1203c7736e125bba30ceaac1915d859dd7043b75
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-06 12:12:55 +0200

    Added WS_FTP 95 codec

---
+++ UniDec.WSFTP95.Codec/Codec.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UniDecAPI;
+
+namespace UniDec.WSFTP95.Codec
+{
+    public class Codec : ICodec
+    {
+        public string FriendlyName { get { return "WS FTP 95"; } }
+        public string CallName { get { return "wsftp95"; } }
+        public bool NeedsKey { get { return false; } }
+
+        public string Decode(string input)
+        {
+            var decodedText = string.Empty;
+
+            for (var i = 0; i < input.Length / 2; i++)
+            {
+                var character = input.Substring(i * 2, 2);
+                var decodedCharacter = Convert.ToInt32(character, 16) - i;
+                decodedText += (char)decodedCharacter;
+            }
+            return decodedText;
+        }
+
+        public string Encode(string input)
+        {
+            var encodedText = string.Empty;
+
+            for (var i = 0; i < input.Length / 2; i++)
+            {
+                var character = input.Substring(i * 2, 2);
+                var encodedCharacter = Convert.ToInt32(character, 16) + i;
+                encodedText += (char)encodedCharacter;
+            }
+            return encodedText;
+        }
+
+        public string Decode(string input, string key)
+        {
+            return "No key required.";
+        }
+
+        public string Encode(string input, string key)
+        {
+            return "No key required.";
+        }
+    }
+}
